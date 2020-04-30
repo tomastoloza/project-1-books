20808e40465df8dcde67d3a754124743d330664b
tomastoloza <tomasezequieltoloza@gmail.com>
2020-04-30 00:34:55 -0300

    Add navbar with login register

--- application.py
+++ application.py
@@ -1,34 +1,61 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 import configparser
+import requests
 
 app = Flask(__name__)
 
 
-def get_credentials(variable):
+def get_credentials(section, variable):
     parser = configparser.ConfigParser()
     parser.read("resources/credentials.ini", encoding='utf-8')
-    return parser.get("CONFIG", variable)
+    return parser.get(section, variable)
 
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
-    os.environ['DATABASE_URL'] = get_credentials("config.uri")
+    os.environ['DATABASE_URL'] = get_credentials("HEROKU", "uri")
 
 # Configure session to use filesystem
+os.environ['FLASK_DEBUG'] = "1"
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+getenv = os.getenv("DATABASE_URL")
+print(getenv)
+engine = create_engine(getenv)
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/")
+# Goodreads request
+# res = requests.get("https://www.goodreads.com/book/review_counts.json",
+#                    params={"key": get_credentials("GOODREADS", "key"), "isbns": "9781632168146"})
+# print(res.json())
+
+@app.route('/')
 def index():
-    return "Project 1: TODO"
+    return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        display_name = request.form.get('input_name')
+        email = request.form.get('input_email')
+        password = request.form.get('input_password')
+        if not password == request.form.get('input_repeated_password'):
+            print("nope")
+        # query = "UPDATE "
+        db.execute("SELECT * FROM database")
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
