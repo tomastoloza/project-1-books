ee8d7775d1bff7df1d9c476f55acfdc475fecc5b
tomastoloza <tomasezequieltoloza@gmail.com>
2020-05-03 18:32:40 -0300

    Add and fix models

--- models.py
+++ models.py
@@ -15,3 +15,17 @@ class Review(db.Model):
     __tablename__ = 'reviews'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+    isbn = db.Column(db.String, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+
+if __name__ == '__main__':
+    from application import app
+    with app.app_context():
+        db.create_all()
